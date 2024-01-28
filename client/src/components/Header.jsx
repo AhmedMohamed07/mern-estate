85027ac01eebfd98133e6d7c824a388e1e6f113e
ahmedmohamed07 <ahmed.mohamed197707@gmail.com>
2024-01-28 06:37:17 +0200

    start

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-sky-700">Ahmed</span>
+          <span className="text-slate-700">Estate</span>
+        </h1>
+
+        <form className="bg-slate-100 p-3 flex items-center rounded-lg">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent w-20 sm:w-64 focus:outline-none"
+          />
+
+          <button>
+            <FaSearch className="text-slate-700" />
+          </button>
+        </form>
+
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700  hover:text-slate-950 hover:underline">
+              Home
+            </li>
+          </Link>
+
+          <Link to="/About">
+            <li className="hidden sm:inline text-slate-700  hover:text-slate-950 hover:underline">
+              About
+            </li>
+          </Link>
+
+          <Link to="/sign-in">
+            <li className="text-slate-700  hover:text-slate-950 hover:underline">
+              Sign in
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
