6bf53faaad5df63a961ffcff10b4d2235919e7d9
ahmedmohamed07 <ahmed.mohamed197707@gmail.com>
2024-02-02 03:05:53 +0200

    complete sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,16 +6,17 @@ export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-sky-700">Ahmed</span>
-          <span className="text-slate-700">Estate</span>
-        </h1>
-
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-sky-700">Ahmed</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
         <form className="bg-slate-100 p-3 flex items-center rounded-lg">
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent w-20 sm:w-64 focus:outline-none"
+            className="bg-transparent w-24 sm:w-64 text-xs md:text-sm focus:outline-none"
           />
 
           <button>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="max-w-lg mx-auto p-3">
+      <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
+
+      <form className="flex flex-col gap-4 ">
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="p-3 rounded-lg border"
+        />
+
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="p-3 rounded-lg border"
+        />
+
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="p-3 rounded-lg border"
+        />
+
+        <button
+          type="submit"
+          className="bg-blue-950 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-70"
+        >
+          Sign up
+        </button>
+      </form>
+
+      <p className="mt-5">
+        Have an account?
+        <Link to="/sign-in" className="text-blue-600 font-bold">
+          {' '}
+          Sign in
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Signup;
