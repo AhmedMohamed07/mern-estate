4afc8d045fa0b60e2a1c3e3b205487e109c2e8ac
ahmedmohamed07 <ahmed.mohamed197707@gmail.com>
2024-02-05 03:02:23 +0200

    complete sign up page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,17 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const changeHandler = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3">
       <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
 
-      <form className="flex flex-col gap-4 ">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
         <input
           type="text"
           id="username"
           placeholder="Username"
           className="p-3 rounded-lg border"
+          onChange={changeHandler}
         />
 
         <input
@@ -19,6 +60,7 @@ const Signup = () => {
           id="email"
           placeholder="Email"
           className="p-3 rounded-lg border"
+          onChange={changeHandler}
         />
 
         <input
@@ -26,13 +68,15 @@ const Signup = () => {
           id="password"
           placeholder="Password"
           className="p-3 rounded-lg border"
+          onChange={changeHandler}
         />
 
         <button
+          disabled={loading}
           type="submit"
           className="bg-blue-950 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-70"
         >
-          Sign up
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
 
@@ -43,6 +87,7 @@ const Signup = () => {
           Sign in
         </Link>
       </p>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 };
