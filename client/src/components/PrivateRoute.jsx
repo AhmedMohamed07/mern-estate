68cee5733ef95c90bf5651e55f5b267b1b2856ac
ahmedmohamed07 <ahmed.mohamed197707@gmail.com>
2024-02-09 22:39:30 +0200

    update the header and make the profile page private

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
 export default function PrivateRoute() {
-  return <div>PrivateRoute</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to={'sign-in'} />;
 }
